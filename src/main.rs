489794fb46a95fcc87fffe57c8d65ad7998a3c6b
cagrigit-hub <cagrikutayok@hotmail.com>
2023-01-29 11:16:47 +0300

    last commit

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::io;
 fn main() {
     let mut input = String::new();
     println!("Enter your weight on Earth");
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    // println!("Our weight on Earth is {}", input);
-    // let jupyter_weight = calculate_weight_on_jupyter(input.parse::<f64>().unwrap());
-    // println!("Our weight on Jupiter is {}", jupyter_weight);
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Your weight on Earth is {}", input);
+    let weight = input.trim().parse::<f64>().unwrap();
+    let jupyter_weight = calculate_weight_on_jupyter(weight);
+    println!("Your weight on Jupiter is {}", jupyter_weight);
 }
 
 fn calculate_weight_on_jupyter(weight : f64) -> f64 {
